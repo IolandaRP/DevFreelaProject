a74c9285eacdfb9356da52cba75a52a0407a7ffa
IolandaRodrigues <iolandarodrigues270@gmail.com>
2023-03-15 11:08:51 -0300

    criando JWT

---
+++ DevFreela.Core/Services/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Core.Services
+{
+    public interface IAuthService
+    {
+        string GenerateJewToken(string email, string role);
+        //role representa o papel do usuário: cliente ou freelancer
+    }
+}
